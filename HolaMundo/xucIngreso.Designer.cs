1c0f2add55533e3202127e27e39277ffb36d6ba1
ethedy <ethedy@gmail.com>
2014-06-08 20:42:23 -0300

    Trolo

--- HolaMundo/xucIngreso.Designer.cs
+++ HolaMundo/xucIngreso.Designer.cs
@@ -28,68 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // textEdit1
-            // 
-            this.textEdit1.Location = new System.Drawing.Point(105, 3);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Size = new System.Drawing.Size(100, 20);
-            this.textEdit1.TabIndex = 0;
-            // 
-            // textEdit2
-            // 
-            this.textEdit2.Location = new System.Drawing.Point(105, 35);
-            this.textEdit2.Name = "textEdit2";
-            this.textEdit2.Size = new System.Drawing.Size(100, 20);
-            this.textEdit2.TabIndex = 1;
-            // 
-            // simpleButton1
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.simpleButton1, 2);
-            this.simpleButton1.Location = new System.Drawing.Point(3, 96);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
-            this.simpleButton1.TabIndex = 2;
-            this.simpleButton1.Text = "simpleButton1";
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.38348F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.61652F));
-            this.tableLayoutPanel1.Controls.Add(this.textEdit1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textEdit2, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.simpleButton1, 2, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(339, 150);
-            this.tableLayoutPanel1.TabIndex = 3;
-            // 
-            // xucIngreso
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "xucIngreso";
-            this.Size = new System.Drawing.Size(339, 150);
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.ResumeLayout(false);
+      this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+      this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+      this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // textEdit1
+      // 
+      this.textEdit1.EditValue = "Puto el que lee";
+      this.textEdit1.Location = new System.Drawing.Point(105, 3);
+      this.textEdit1.Name = "textEdit1";
+      this.textEdit1.Size = new System.Drawing.Size(219, 20);
+      this.textEdit1.TabIndex = 0;
+      // 
+      // textEdit2
+      // 
+      this.textEdit2.Location = new System.Drawing.Point(105, 35);
+      this.textEdit2.Name = "textEdit2";
+      this.textEdit2.Size = new System.Drawing.Size(100, 20);
+      this.textEdit2.TabIndex = 1;
+      // 
+      // simpleButton1
+      // 
+      this.tableLayoutPanel1.SetColumnSpan(this.simpleButton1, 2);
+      this.simpleButton1.Location = new System.Drawing.Point(3, 96);
+      this.simpleButton1.Name = "simpleButton1";
+      this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+      this.simpleButton1.TabIndex = 2;
+      this.simpleButton1.Text = "simpleButton1";
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 2;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.38348F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.61652F));
+      this.tableLayoutPanel1.Controls.Add(this.textEdit1, 1, 0);
+      this.tableLayoutPanel1.Controls.Add(this.textEdit2, 1, 1);
+      this.tableLayoutPanel1.Controls.Add(this.simpleButton1, 2, 2);
+      this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 3;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(339, 150);
+      this.tableLayoutPanel1.TabIndex = 3;
+      // 
+      // xucIngreso
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Name = "xucIngreso";
+      this.Size = new System.Drawing.Size(339, 150);
+      ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.ResumeLayout(false);
 
         }
 
